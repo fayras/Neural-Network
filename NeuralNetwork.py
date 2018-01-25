7c567531aa9a155927853553585fb9d0442687c1
Dimitri Tarnavski <dimitri.tarn@gmail.com>
2018-01-25 20:22:30 +0100

    Es werden nun Gewichte des Netzwerks angelegt. Man hat dabei die Wahl zwischen klassisch komplett zufällig, als auch Gewichte zufällig einer Normalverteilung nach.

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -8,10 +8,13 @@ class NeuralNetwork:
         self.hNodes = hiddennodes
         self.oNodes = outputnodes
 
+        # Die Lernrate, wie schnell die Gewichte angepasst werden.
         self.lr = learningrate
 
-        self.wih = numpy.random.rand(self.hNodes, self.iNodes) - 0.5
-        self.who = numpy.random.rand(self.oNodes, self.hNodes) - 0.5
+        # Die Gewichte des Netzwerks zwischen den einzelnen
+        # Knotenschichten.
+        self.wih = NeuralNetwork.normal_weights(self.hNodes, self.iNodes)
+        self.who = NeuralNetwork.random_weights(self.oNodes, self.hNodes)
         pass
 
     def train(self):
@@ -19,4 +22,21 @@ class NeuralNetwork:
 
     def query(self):
         pass
+
+    @staticmethod
+    def random_weights(rows, cols):
+
+        # Liefert komplett zufällige Gewichte zurück.
+        # Diese befinden sind zwischen -0.5 und 0.5
+
+        return numpy.random.rand(rows, cols) - 0.5
+
+    @staticmethod
+    def normal_weights(rows, cols):
+
+        # Liefert eine Matrix der Größe mit rows x cols.
+        # Dabei werden die Gewichte nicht zufällig gewählt,
+        # sondern werden nach einer Normalverteilung gewählt.
+
+        return numpy.random.normal(0.0, pow(rows, -0.5), (rows, cols))
 
