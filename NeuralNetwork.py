f3ba60370420a243cc9d94548de3733b8d3340dc
Dimitri Tarnavski <dimitri.tarn@gmail.com>
2018-02-14 19:56:00 +0100

    Die Methoden für Abfragen und Trainieren des Netzwerks hinzugefügt.

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -1,4 +1,5 @@
 import numpy
+import scipy.special
 
 
 class NeuralNetwork:
@@ -15,17 +16,43 @@ class NeuralNetwork:
         # Knotenschichten.
         self.wih = NeuralNetwork.normal_weights(self.hNodes, self.iNodes)
         self.who = NeuralNetwork.random_weights(self.oNodes, self.hNodes)
+
+        # Die Aktivierungsfunktion für das Netzwerk
+        self.activation_function = lambda x: scipy.special.expit(x)
+
         pass
 
-    def train(self):
+    def train(self, inputs_list, targets_list):
+        targets = numpy.array(targets_list, ndmin=2).T
+        inputs = numpy.array(inputs_list, ndmin=2).T
+
+        hidden_inputs = numpy.dot(self.wih, inputs)
+        hidden_outputs = self.activation_function(hidden_inputs)
+
+        final_inputs = numpy.dot(self.who, hidden_outputs)
+        final_outputs = self.activation_function(final_inputs)
+
+        output_errors = targets - final_outputs
+        hidden_errors = numpy.dot(self.who.T, output_errors)
+
+        self.who += self.lr * numpy.dot(output_errors * final_outputs * (1.0 - final_outputs), numpy.transpose(hidden_outputs))
+        self.wih += self.lr * numpy.dot(hidden_errors * hidden_outputs * (1.0 - hidden_outputs), numpy.transpose(inputs))
+
         pass
 
-    def query(self):
-        pass
+    def query(self, inputs_list):
+        inputs = numpy.array(inputs_list, ndmin=2).T
+
+        hidden_inputs = numpy.dot(self.wih, inputs)
+        hidden_outputs = self.activation_function(hidden_inputs)
+
+        final_inputs = numpy.dot(self.who, hidden_outputs)
+        final_outputs = self.activation_function(final_inputs)
+
+        return final_outputs
 
     @staticmethod
     def random_weights(rows, cols):
-
         # Liefert komplett zufällige Gewichte zurück.
         # Diese befinden sind zwischen -0.5 und 0.5
 
@@ -33,10 +60,8 @@ class NeuralNetwork:
 
     @staticmethod
     def normal_weights(rows, cols):
-
         # Liefert eine Matrix der Größe mit rows x cols.
         # Dabei werden die Gewichte nicht zufällig gewählt,
         # sondern werden nach einer Normalverteilung gewählt.
 
         return numpy.random.normal(0.0, pow(rows, -0.5), (rows, cols))
-
